e93d07b9bc76daa0cd5e2032ac404ff119e9765f
Eduardo1865 <eduardo.a@gec.inatel.br>
2025-07-15 21:46:58 -0300

    new file:   css/styleMonitor.css 	renamed:    images/caminho1.gif -> images/esqueleto.gif 	modified:   index.html 	new file:   javascript/monitor.js 	new file:   monitor.html

---
+++ javascript/monitor.js
@@ -0,0 +1,23 @@
+
+
+function selectCaminho(){
+    let monitorTextBox = document.getElementById("monitorTextBox").value;
+    const caminhoImagem = document.getElementById("caminhoImagem");
+    const imagem = '';
+    
+
+    monitorTextBox = monitorTextBox.toLowerCase();
+    
+    switch(monitorTextBox){
+        case "esqueleto":
+            const value = monitorTextBox;
+            caminhoImagem.innerHTML = `<img src="../images/${value}.gif" height=120px alt="caminho${value}" class="monitorImagem" >`;
+            console.log(value);
+            break;
+        
+        default:
+            console.log("nope")
+            caminhoImagem.innerHTML = `<img src="../images/monitor.gif" height=120px class="monitorImagem" >`
+            break;
+    }
+}
